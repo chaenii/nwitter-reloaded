8edd159db30228053ccc69b6a8b7a1d9570678bb
chaenii <chaenii.jeon@gmail.com>
2024-06-22 18:04:38 +0900

    setup firebase project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfJwIHi45_hBjUqH-q9MPlRgzml4GED8U",
+  authDomain: "nwitter-reloaded-aadf6.firebaseapp.com",
+  projectId: "nwitter-reloaded-aadf6",
+  storageBucket: "nwitter-reloaded-aadf6.appspot.com",
+  messagingSenderId: "357952013142",
+  appId: "1:357952013142:web:889722660a33dfb13d8cf7",
+};
+
+const app = initializeApp(firebaseConfig);
